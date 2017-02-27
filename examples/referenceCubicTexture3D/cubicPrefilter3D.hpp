d52d23cf4ad152d011101b19d305d64540b665fd
DannyRuijters <services@dannyruijters.nl>
2017-02-27 16:52:51 +0100

    Update cubicPrefilter3D.hpp
    small bug fix in SamplesToCoefficients3DZ

--- examples/referenceCubicTexture3D/cubicPrefilter3D.hpp
+++ examples/referenceCubicTexture3D/cubicPrefilter3D.hpp
@@ -32,7 +32,7 @@ policies, either expressed or implied.
 
 When using this code in a scientific project, please cite one or all of the
 following papers:
-*  Daniel Ruijters and Philippe Thévenaz,
+*  Daniel Ruijters and Philippe ThÃ©venaz,
    GPU Prefilter for Accurate Cubic B-Spline Interpolation, 
    The Computer Journal, vol. 55, no. 1, pp. 15-20, January 2012.
    http://dannyruijters.nl/docs/cudaPrefilter3.pdf
@@ -82,7 +82,7 @@ void SamplesToCoefficients3DZ(
 	// process lines in z-direction
 	const uint startIdx = y * width + x;
 	const uint slice = height * width;
-	ConvertToInterpolationCoefficients(volume + startIdx, height, slice * sizeof(float));
+	ConvertToInterpolationCoefficients(volume + startIdx, depth, slice * sizeof(float));
 }
 
 //--------------------------------------------------------------------------
